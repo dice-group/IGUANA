45c4989a53e3e76348f10ae4001aa0ee4f277877
Felix Conrads <felixconrads@googlemail.com>
2015-11-06 17:04:56 +0100

    MANUAL update and log DEBUG enabled

--- de.uni_leipzig.iguana/src/main/java/de/uni_leipzig/iguana/benchmark/Main.java
+++ de.uni_leipzig.iguana/src/main/java/de/uni_leipzig/iguana/benchmark/Main.java
@@ -37,16 +37,16 @@ public class Main {
 	}
 
 	private static void initJena() {
-		org.apache.log4j.Logger.getLogger(
-				"log4j.logger.org.apache.jena.arq.info").setLevel(
-				org.apache.log4j.Level.OFF);
-		org.apache.log4j.Logger.getLogger(
-				"log4j.logger.org.apache.jena.arq.exec").setLevel(
-				org.apache.log4j.Level.OFF);
-		org.apache.log4j.Logger.getLogger("log4j.logger.org.apache.jena")
-				.setLevel(org.apache.log4j.Level.OFF);
-		org.apache.log4j.Logger.getRootLogger().setLevel(
-				org.apache.log4j.Level.OFF);
+//		org.apache.log4j.Logger.getLogger(
+//				"log4j.logger.org.apache.jena.arq.info").setLevel(
+//				org.apache.log4j.Level.OFF);
+//		org.apache.log4j.Logger.getLogger(
+//				"log4j.logger.org.apache.jena.arq.exec").setLevel(
+//				org.apache.log4j.Level.OFF);
+//		org.apache.log4j.Logger.getLogger("log4j.logger.org.apache.jena")
+//				.setLevel(org.apache.log4j.Level.OFF);
+//		org.apache.log4j.Logger.getRootLogger().setLevel(
+//				org.apache.log4j.Level.OFF);
 		ConnectionFactory
 				.setDriver("org.apache.jena.jdbc.remote.RemoteEndpointDriver");
 		ConnectionFactory.setJDBCPrefix("jdbc:jena:remote:query=http://");
