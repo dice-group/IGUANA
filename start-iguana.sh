6739a1cb952009d56693ae4a6c451313951b6d23
F. Conrads <felixconrads@googlemail.com>
2019-05-28 16:41:13 +0200

    Update start-iguana.sh

--- start-iguana.sh
+++ start-iguana.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -cp iguana.resultprocessor/target/iguana.resultprocessor-2.0.0.jar org.aksw.iguana.rp.controller.MainController iguana.resultprocessor/src/main/resources/iguana.properties &
-java -cp iguana.corecontroller/target/iguana.corecontroller-2.0.0.jar org.aksw.iguana.cc.controller.MainController iguana.corecontroller/src/main/resources/iguana.properties &
+java -cp iguana.resultprocessor/target/iguana.resultprocessor-2.1.0.jar org.aksw.iguana.rp.controller.MainController iguana.resultprocessor/src/main/resources/iguana.properties &
+java -cp iguana.corecontroller/target/iguana.corecontroller-2.1.0.jar org.aksw.iguana.cc.controller.MainController iguana.corecontroller/src/main/resources/iguana.properties &
