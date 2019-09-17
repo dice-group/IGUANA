abc89271183f7e90ca773b1b798de8fac00ad498
TortugaAttack <felixconrads@googlemail.com>
2019-09-17 17:53:58 +0200

    tool update

---
+++ iguana.resultprocessor/src/main/java/org/aksw/iguana/rp/tools/Field.java
@@ -0,0 +1,51 @@
+package org.aksw.iguana.rp.tools;
+
+public enum Field {
+	_queryID("", "", "?query <http://iguana-benchmark.eu/properties/queryID> ?queryID ."),
+	queryID("Query ID", "?queryID", "?query <http://iguana-benchmark.eu/properties/queryID> ?queryID ."),
+	_query("", "", "?uuid <http://iguana-benchmark.eu/properties/qps#query> ?query . "),
+	query("Query", "?query", "?uuid <http://iguana-benchmark.eu/properties/qps#query> ?query . "),
+	_qps("", "", " ?query <http://iguana-benchmark.eu/properties/queriesPerSecond> ?queriesPerSecond ."),
+	qps("qps", "?qps", " ?query <http://iguana-benchmark.eu/properties/queriesPerSecond> ?qps ."),
+	_dataset("", "", " ?expID <http://iguana-benchmark.eu/properties/dataset> ?dataset ."),
+	dataset("Dataset", "?dataset", " ?expID <http://iguana-benchmark.eu/properties/dataset> ?dataset ."),
+	_connection("", "", " ?taskID <http://iguana-benchmark.eu/properties/connection> ?connection ."),
+	connection("Triplestore", "?connection", " ?taskID <http://iguana-benchmark.eu/properties/connection> ?connection ."),
+	//No_clients succeded	failed	totaltime	result size
+	succeded("succeded", "?succeded", " ?query <http://iguana-benchmark.eu/properties/succeded> ?succeded ."),
+	failed("failed", "?failed", " ?query <http://iguana-benchmark.eu/properties/failed> ?failed ."),
+	totaltime("totaltime", "?totaltime", " ?query <http://iguana-benchmark.eu/properties/totalTime> ?totaltime ."),
+	resultsize("resultsize", "?resultsize", " ?query <http://iguana-benchmark.eu/properties/resultSize> ?resultsize ."),
+	workers("No_clients", "?workers", " ?expID <http://iguana-benchmark.eu/properties/noOfWorkers> ?workers ."),
+	workers_1("No_clients", "(\"1\" AS ?workers)", ""),
+	workers_4("No_clients", "(\"4\" AS ?workers)", ""),
+	workers_8("No_clients", "(\"8\" AS ?workers)", ""),
+	workers_16("No_clients", "(\"16\" AS ?workers)", ""),
+	workers_32("No_clients", "(\"32\" AS ?workers)", ""),
+
+	empty("Empty", "(\"x\" AS ?empty)", ""),
+	workerID("Worker ID", "?workerID", "?uuid  <http://iguana-benchmark.eu/properties/workerID> ?workerID"),
+	http_format("FORMAT", "(\"HTTP\" AS ?format)", ""),
+	cli_format("FORMAT", "(\"CLI\" AS ?format)", "");
+	private String value;
+	private String var;
+	private String triples;
+
+	Field(String value, String var, String triples) {
+		this.value = value;
+		this.var=var;
+		this.triples=triples;
+	}
+
+	public String getValue() {
+		return value;
+	}
+	public String getVar() {
+		return var;
+	}
+	public String getTriples() {
+		return triples;
+	}
+	
+
+}
---
+++ iguana.resultprocessor/src/main/java/org/aksw/iguana/rp/tools/RDF2CSV.java
@@ -0,0 +1,110 @@
+package org.aksw.iguana.rp.tools;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+
+public class RDF2CSV {
+
+	public static void main(String args[]) throws FileNotFoundException {
+		if(args.length<3) {
+			System.out.println("Usage: iguana2csv.sh <Iguana Results File> <Output File> <Fields> <TaskID URI>+");
+			System.out.println("\t - Fields: Field;Field;...;(http_format | cli_format)*;... ");
+			System.out.println("\t - Field: (qps|query|queryID|) ");
+			System.out.println("\t \t  you can also use _Field to use it as a bridge but not have it in the results");
+			System.out.println("\t \t  f.e. _query;qps will lead to the qps for all queries in the given experiment uris");
+			return;
+		}
+		try (PrintWriter pw = new PrintWriter(new FileOutputStream(args[1], true))) {
+			Field[] fields = readFields(args[2]);
+			StringBuilder header = new StringBuilder("Benchmark ID;");
+			for (Field f : fields) {
+				header.append(f.getValue()).append(";");
+			}
+			pw.println(header.toString());
+			for(int i=3;i<args.length;i++) {
+				convertToCSV(pw, readIguanaResultFile(args[0]),args[i], fields);
+			}
+		}
+	}
+
+	private static Field[] readFields(String string) {
+		String[] fieldNames = string.split(";");
+		Field[] fields = new Field[fieldNames.length];
+		for(int i=0;i<fieldNames.length;i++) {
+			fields[i] = Field.valueOf(fieldNames[i]);
+		}
+		return fields;
+	}
+
+	public static Model readIguanaResultFile(String file) throws FileNotFoundException {
+		return ModelFactory.createDefaultModel().read(new FileInputStream(file), null, "N-TRIPLE");
+	}
+
+	/**
+	 * Creates a CSV file using the give fields in that order
+	 * 
+	 * @param fields
+	 */
+	public static void convertToCSV(PrintWriter pw, Model m, String taskID, Field... fields) {
+		QueryExecution qexec = QueryExecutionFactory.create(createQuery(taskID, fields), m);
+		ResultSet res = qexec.execSelect();
+
+		while (res.hasNext()) {
+			StringBuilder line = new StringBuilder(taskID.replace("<http://iguana-benchmark.eu/recource/", "").replace(">", "")).append(";");
+			QuerySolution sol = res.next();
+			for(Field f : fields) {
+				if(!f.getVar().isEmpty()) {
+					if(Field.http_format.equals(f) || Field.cli_format.equals(f)) {
+						String var =  "format";
+						line.append(sol.get(var)).append(";");						
+					}
+					else if (Field.empty.equals(f)) {
+						line.append(";");
+					}
+					else if(Field.workers_1.equals(f)||Field.workers_4.equals(f)||Field.workers_8.equals(f)||Field.workers_16.equals(f)||Field.workers_32.equals(f)) {
+						String var =  "workers";
+						line.append(sol.get(var)).append(";");	
+					}
+					else {
+						String var = f.getVar().substring(1);
+						line.append(getValueOfNode(sol.get(var))).append(";");
+					}
+				}
+			}
+			pw.println(line.toString());
+		}
+	}
+
+	private static String getValueOfNode(RDFNode rdfNode) {
+		if(rdfNode.isLiteral()) {
+			return rdfNode.asLiteral().getValue().toString();
+		}
+		return rdfNode.asResource().getURI().replace("http://iguana-benchmark.eu/recource/", "").replace("http://iguana-benchmark.eu/properties/", "");
+	}
+
+	private static Query createQuery(String taskID, Field[] fields) {
+		StringBuilder vars = new StringBuilder();
+		StringBuilder triples = new StringBuilder(taskID).append(" ?p ?uuid . ");
+		triples.append("?expID <http://iguana-benchmark.eu/properties/task> "+taskID+" .");
+		triples.append("?suiteId <http://iguana-benchmark.eu/properties/experiment> ?expID .");
+		triples.append("?expID <http://iguana-benchmark.eu/properties/task> ?taskID FILTER(?taskID="+taskID+").");
+		for (Field field : fields) {
+			vars.append(field.getVar()).append(" ");
+			triples.append(field.getTriples()).append(" ");
+		}
+		return QueryFactory.create("SELECT DISTINCT " + vars + " { " + triples + " }");
+	}
+}
