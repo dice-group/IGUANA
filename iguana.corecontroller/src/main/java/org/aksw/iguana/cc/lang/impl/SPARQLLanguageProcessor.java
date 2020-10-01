aaf3fc982f6832dce937918d6ee22de256a2b4df
TortugaAttack <felixconrads@googlemail.com>
2020-10-01 12:13:48 +0200

    Added lsq owl:sameAs #80

--- iguana.corecontroller/src/main/java/org/aksw/iguana/cc/lang/impl/SPARQLLanguageProcessor.java
+++ iguana.corecontroller/src/main/java/org/aksw/iguana/cc/lang/impl/SPARQLLanguageProcessor.java
@@ -99,23 +99,11 @@ public class SPARQLLanguageProcessor implements LanguageProcessor {
     }
 
     private Resource getLSQHash(Query query){
-        String result = MD5(query.toString()).substring(0, 8);;
+        HashCode hashCode = Hashing.sha256().hashString(query.toString(), StandardCharsets.UTF_8);
+        String result = BaseEncoding.base64Url().omitPadding().encode(hashCode.asBytes());
         return ResourceFactory.createResource(LSQ_RES+result);
     }
 
-    private String MD5(String md5) {
-        try {
-            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-            byte[] array = md.digest(md5.getBytes());
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
-            }
-            return sb.toString();
-        } catch (java.security.NoSuchAlgorithmException e) {
-        }
-        return null;
-    }
 
     public static String getContentTypeVal(Header header) {
         for (HeaderElement el : header.getElements()) {
