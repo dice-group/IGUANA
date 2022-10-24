f533fea27ea0a7c038653bacdd224397058686e8
frensing <dev@frensing.de>
2022-10-24 17:36:13 +0300

    FileLineQuerySourceTest

---
+++ iguana.corecontroller/src/test/java/org/aksw/iguana/cc/query/source/FileLineQuerySourceTest.java
@@ -0,0 +1,42 @@
+package org.aksw.iguana.cc.query.source;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileLineQuerySourceTest {
+
+    private static final String PATH = "src/test/resources/query/source/queries.txt";
+
+    private final FileLineQuerySource querySource;
+
+    public FileLineQuerySourceTest() throws IOException {
+        this.querySource = new FileLineQuerySource(PATH);
+    }
+
+    @Test
+    public void size() {
+        assertEquals(3, this.querySource.size());
+    }
+
+    @Test
+    public void getQuery() throws IOException {
+        assertEquals("QUERY 1 {still query 1}", this.querySource.getQuery(0));
+        assertEquals("QUERY 2 {still query 2}", this.querySource.getQuery(1));
+        assertEquals("QUERY 3 {still query 3}", this.querySource.getQuery(2));
+    }
+
+    @Test
+    public void getAllQueries() throws IOException {
+        List<String> expected = new ArrayList<>(3);
+        expected.add("QUERY 1 {still query 1}");
+        expected.add("QUERY 2 {still query 2}");
+        expected.add("QUERY 3 {still query 3}");
+
+        assertEquals(expected, this.querySource.getAllQueries());
+    }
+}
