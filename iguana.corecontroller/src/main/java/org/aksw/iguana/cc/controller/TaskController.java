f2a5699f745af0c393150b46c0b7905d9a2965d7
frensing <dev@frensing.de>
2022-10-25 23:21:22 +0300

    cleanup

--- iguana.corecontroller/src/main/java/org/aksw/iguana/cc/controller/TaskController.java
+++ iguana.corecontroller/src/main/java/org/aksw/iguana/cc/controller/TaskController.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.aksw.iguana.cc.controller;
 
 import org.aksw.iguana.cc.config.elements.Connection;
@@ -11,37 +8,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 
 /**
  * Task Controlling, will start the actual benchmark tasks and its {@link org.aksw.iguana.cc.tasks.TaskManager}
  *
- *
- * 
  * @author f.conrads
- *
  */
 public class TaskController {
 
-	private static Map<String, String> shortHandMap = new HashMap<String, String>();
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(TaskController.class);
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(TaskController.class);
-
-	public void startTask(String[] ids, String dataset, Connection con, Task task) {
-		TaskManager tmanager = new TaskManager();
-		String className=task.getClassName();
-		TaskFactory factory = new TaskFactory();
-		tmanager.setTask(factory.create(className, task.getConfiguration()));
-		try {
-			tmanager.startTask(ids, dataset, con, task.getName());
-		} catch (IOException | TimeoutException e) {
-			LOGGER.error("Could not start Task "+className, e);
-		}
-	}
-
-
+    public void startTask(String[] ids, String dataset, Connection con, Task task) {
+        TaskManager tmanager = new TaskManager();
+        String className = task.getClassName();
+        TaskFactory factory = new TaskFactory();
+        tmanager.setTask(factory.create(className, task.getConfiguration()));
+        try {
+            tmanager.startTask(ids, dataset, con, task.getName());
+        } catch (IOException | TimeoutException e) {
+            LOGGER.error("Could not start Task " + className, e);
+        }
+    }
 }
