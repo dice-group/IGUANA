0d17a6949a6f615266589c3708c4d9e2f3c031d1
frensing <dev@frensing.de>
2022-10-31 19:24:26 +0200

    refactor cliworkers to use new query handling

--- iguana.corecontroller/src/main/java/org/aksw/iguana/cc/worker/impl/CLIInputWorker.java
+++ iguana.corecontroller/src/main/java/org/aksw/iguana/cc/worker/impl/CLIInputWorker.java
@@ -3,7 +3,7 @@ package org.aksw.iguana.cc.worker.impl;
 import org.aksw.iguana.cc.config.elements.Connection;
 import org.aksw.iguana.cc.model.QueryExecutionStats;
 import org.aksw.iguana.cc.utils.CLIProcessManager;
-import org.aksw.iguana.cc.worker.AbstractRandomQueryChooserWorker;
+import org.aksw.iguana.cc.worker.AbstractWorker;
 import org.aksw.iguana.commons.annotation.Nullable;
 import org.aksw.iguana.commons.annotation.Shorthand;
 import org.aksw.iguana.commons.constants.COMMON;
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.Random;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -23,115 +23,102 @@ import static org.aksw.iguana.commons.time.TimeUtils.durationInMilliseconds;
 
 /**
  * Worker to execute a query against a CLI process, the connection.service will be the command to execute the query against.
- *
+ * <p>
  * Assumes that the CLI process won't stop but will just accepts queries one after another and returns the results in the CLI output.
- *
  */
 @Shorthand("CLIInputWorker")
-public class CLIInputWorker extends AbstractRandomQueryChooserWorker {
+public class CLIInputWorker extends AbstractWorker {
 
-	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private final String initFinished;
+    private final String queryFinished;
+    private final String error;
+    private Process process;
 
-	private Random queryChooser;
-	private Process process;
-	private String initFinished;
-	private String queryFinished;
-	private String error;
+    public CLIInputWorker(String taskID, Integer workerID, Connection connection, Map<Object, Object> queries, @Nullable Integer timeLimit, @Nullable Integer timeOut, @Nullable Integer fixedLatency, @Nullable Integer gaussianLatency, String initFinished, String queryFinished, String queryError) {
+        super(taskID, workerID, connection, queries, timeLimit, timeOut, fixedLatency, gaussianLatency);
+        this.initFinished = initFinished;
+        this.queryFinished = queryFinished;
+        this.error = queryError;
+        this.setWorkerProperties();
+    }
 
-	public CLIInputWorker(String taskID, Connection connection, String queriesFile, String initFinished, String queryFinished, String queryError, @Nullable Integer timeOut, @Nullable Integer timeLimit, @Nullable Integer fixedLatency, @Nullable Integer gaussianLatency, Integer workerID) {
-		super(taskID, connection, queriesFile, timeOut, timeLimit, fixedLatency, gaussianLatency, workerID);
-		queryChooser = new Random(this.workerID);
-		this.initFinished = initFinished;
-		this.queryFinished = queryFinished;
-		this.error = queryError;
-		this.setWorkerProperties();
+    private void setWorkerProperties() {
+        // Create a CLI process, initialize it
+        this.process = CLIProcessManager.createProcess(this.con.getEndpoint());
+        try {
+            CLIProcessManager.countLinesUntilStringOccurs(process, this.initFinished, this.error); //Init
+        } catch (IOException e) {
+            LOGGER.error("Exception while trying to wait for init of CLI Process", e);
+        }
+    }
 
-	}
+    @Override
+    public void executeQuery(String query, String queryID) {
+        Instant start = Instant.now();
 
-	private void setWorkerProperties()
-	{
-		queryChooser = new Random(this.workerID);
+        try {
+            // Create background thread that will watch the output of the process and prepare results
+            AtomicLong size = new AtomicLong(-1);
+            AtomicBoolean failed = new AtomicBoolean(false);
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute(() -> {
+                try {
+                    LOGGER.debug("Process Alive: {}", this.process.isAlive());
+                    LOGGER.debug("Reader ready: {}", CLIProcessManager.isReaderReady(this.process));
+                    size.set(CLIProcessManager.countLinesUntilStringOccurs(this.process, this.queryFinished, this.error));
+                } catch (IOException e) {
+                    failed.set(true);
+                }
+            });
 
-		// Create a CLI process, initialize it
-		this.process = CLIProcessManager.createProcess(this.con.getEndpoint());
-		try {
-			CLIProcessManager.countLinesUntilStringOccurs(process, this.initFinished, this.error); //Init
-		} catch (IOException e) {
-			LOGGER.error("Exception while trying to wait for init of CLI Process",e);
-		}
-	}
+            // Execute the query on the process
+            try {
+                if (this.process.isAlive()) {
+                    CLIProcessManager.executeCommand(this.process, writableQuery(query));
+                } else if (this.endSignal) {
+                    super.addResults(new QueryExecutionStats(queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, durationInMilliseconds(start, Instant.now())));
+                    return;
+                } else {
+                    super.addResults(new QueryExecutionStats(queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, durationInMilliseconds(start, Instant.now())));
+                    return;
+                }
+            } finally {
+                executor.shutdown();
+                executor.awaitTermination((long) (double) this.timeOut, TimeUnit.MILLISECONDS);
+            }
 
-	@Override
-	public void executeQuery(String query, String queryID) {
-		Instant start = Instant.now();
+            // At this point, query is executed and background thread has processed the results.
+            // Next, calculate time for benchmark.
+            double duration = durationInMilliseconds(start, Instant.now());
 
-		try {
-			// Create background thread that will watch the output of the process and prepare results
-			AtomicLong size = new AtomicLong(-1);
-			AtomicBoolean failed = new AtomicBoolean(false);
-			ExecutorService executor = Executors.newSingleThreadExecutor();
-			executor.execute(new Runnable() {
+            if (duration >= this.timeOut) {
+                super.addResults(new QueryExecutionStats(queryID, COMMON.QUERY_SOCKET_TIMEOUT, duration));
+                return;
+            } else if (failed.get()) {
+                super.addResults(new QueryExecutionStats(queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, duration));
+                return;
+            }
 
-				@Override
-				public void run() {
-					try {
-						LOGGER.debug("Process Alive: {}", process.isAlive());
-						LOGGER.debug("Reader ready: {}", CLIProcessManager.isReaderReady(process));
-						size.set(CLIProcessManager.countLinesUntilStringOccurs(process, queryFinished, error));
-					} catch (IOException e) {
-						failed.set(true);
-					}
-				}
-			});
-
-			// Execute the query on the process
-			try {
-				if (process.isAlive()) {
-					CLIProcessManager.executeCommand(process, writableQuery(query));
-				} else if (this.endSignal) {
-					super.addResults(new QueryExecutionStats (queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, durationInMilliseconds(start, Instant.now()) ));
-					return;
-				} else {
-					super.addResults(new QueryExecutionStats (queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, durationInMilliseconds(start, Instant.now()) ));
-					return;
-				}
-			} finally {
-				executor.shutdown();
-				executor.awaitTermination((long)(double)this.timeOut, TimeUnit.MILLISECONDS);
-			}
-
-			// At this point, query is executed and background thread has processed the results.
-			// Next, calculate time for benchmark.
-			double duration = durationInMilliseconds(start, Instant.now());
-
-			if (duration >= timeOut) {
-				super.addResults(new QueryExecutionStats (queryID, COMMON.QUERY_SOCKET_TIMEOUT, duration ));
-				return;
-			} else if (failed.get()) {
-				super.addResults(new QueryExecutionStats (queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, duration ));
-				return;
-			}
-
-			// SUCCESS
-			LOGGER.debug("Query successfully executed size: {}", size.get());
-			super.addResults(new QueryExecutionStats (queryID, COMMON.QUERY_SUCCESS, duration, size.get() ));
-		} catch (IOException | InterruptedException e) {
-			LOGGER.warn("Exception while executing query ",e);
-			// ERROR
-			super.addResults(new QueryExecutionStats (queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, durationInMilliseconds(start, Instant.now()) ));
-		}
-	}
+            // SUCCESS
+            LOGGER.debug("Query successfully executed size: {}", size.get());
+            super.addResults(new QueryExecutionStats(queryID, COMMON.QUERY_SUCCESS, duration, size.get()));
+        } catch (IOException | InterruptedException e) {
+            LOGGER.warn("Exception while executing query ", e);
+            // ERROR
+            super.addResults(new QueryExecutionStats(queryID, COMMON.QUERY_UNKNOWN_EXCEPTION, durationInMilliseconds(start, Instant.now())));
+        }
+    }
 
 
-	protected String writableQuery(String query) {
-		return query;
-	}
+    protected String writableQuery(String query) {
+        return query;
+    }
 
 
-
-	@Override
-	public void stopSending() {
-		super.stopSending();
-		process.destroyForcibly();
-	}
+    @Override
+    public void stopSending() {
+        super.stopSending();
+        this.process.destroyForcibly();
+    }
 }
