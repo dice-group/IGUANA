5848fe27d7fac3852171de866d0efb3693fb05cd
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-04-18 21:06:50 +0200

    restructure FileUtilsTest

--- iguana.corecontroller/src/test/java/org/aksw/iguana/cc/utils/FileUtilsTest.java
+++ iguana.corecontroller/src/test/java/org/aksw/iguana/cc/utils/FileUtilsTest.java
@@ -1,85 +1,68 @@
 package org.aksw.iguana.cc.utils;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Enclosed.class)
 public class FileUtilsTest {
 
-    @Test
-    public void countLinesTest() throws IOException {
-        //get test file
-        File f = new File("src/test/resources/fileUtils.txt");
-        //count lines
+    @RunWith(Parameterized.class)
+    public static class ParameterizedTest {
+        private final File file;
 
-        long startTime = 0;
-        long endTime = 0;
+        public ParameterizedTest(File file) {
+            this.file = file;
+        }
 
-        startTime = System.nanoTime();
-        assertEquals(6, FileUtils.countLines(f));
-        endTime = System.nanoTime();
-        System.out.println(((double) (endTime - startTime) / 1000000) + "ms");
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            File f1 = new File("src/test/resources/readLineTestFile1.txt");
+            File f2 = new File("src/test/resources/readLineTestFile2.txt");
+            File f3 = new File("src/test/resources/readLineTestFile3.txt");
 
-        File f1 = new File("src/test/resources/readLineTestFile1.txt");
-        File f2 = new File("src/test/resources/readLineTestFile2.txt");
-        File f3 = new File("src/test/resources/readLineTestFile3.txt");
-        startTime = System.nanoTime();
-        assertEquals(4, FileUtils.countLines(f1));
-        endTime = System.nanoTime();
-        System.out.println(((double) (endTime - startTime) / 1000000) + "ms");
-        startTime = System.nanoTime();
-        assertEquals(4, FileUtils.countLines(f2));
-        endTime = System.nanoTime();
-        System.out.println(((double) (endTime - startTime) / 1000000) + "ms");
-        startTime = System.nanoTime();
-        assertEquals(4, FileUtils.countLines(f3));
-        endTime = System.nanoTime();
-        System.out.println(((double) (endTime - startTime) / 1000000) + "ms");
+            return Arrays.asList(new Object[][]{
+                    {f1},
+                    {f2},
+                    {f3}
+            });
+        }
+
+        @Test
+        public void countLinesTest() throws IOException {
+            assertEquals(4, FileUtils.countLines(this.file));
+        }
+
+        @Test
+        public void readLineAtTest() throws IOException {
+            assertEquals("line 1", FileUtils.readLineAt(0, this.file));
+            assertEquals("line 2", FileUtils.readLineAt(1, this.file));
+            assertEquals("line 3", FileUtils.readLineAt(2, this.file));
+            assertEquals("line 4", FileUtils.readLineAt(3, this.file));
+        }
+
+        @Test
+        public void getHashTest(){
+            //check if hash abs works
+            assertTrue(FileUtils.getHashcodeFromFileContent(this.file.getAbsolutePath())>0);
+        }
     }
 
-    @Test
-    public void readLineAtTest() throws IOException {
-        //get test file
-        File f = new File("src/test/resources/fileUtils.txt");
-        //read line at 2, 15
-        assertEquals("a", FileUtils.readLineAt(0, f));
-        assertEquals("abc", FileUtils.readLineAt(2, f));
-        //is at actual line 16, but as all the lines between line 4-10 and 12-15 are empty this should be the 4th
-        assertEquals("dfe", FileUtils.readLineAt(4, f));
-        //read line at -1
-        assertEquals("", FileUtils.readLineAt(-1, f));
-
-        File f1 = new File("src/test/resources/readLineTestFile1.txt");
-        File f2 = new File("src/test/resources/readLineTestFile2.txt");
-        File f3 = new File("src/test/resources/readLineTestFile3.txt");
-        assertEquals("line 1", FileUtils.readLineAt(0, f1));
-        assertEquals("line 1", FileUtils.readLineAt(0, f2));
-        assertEquals("line 1", FileUtils.readLineAt(0, f3));
-        assertEquals("line 2", FileUtils.readLineAt(1, f1));
-        assertEquals("line 2", FileUtils.readLineAt(1, f2));
-        assertEquals("line 2", FileUtils.readLineAt(1, f3));
-        assertEquals("line 3", FileUtils.readLineAt(2, f1));
-        assertEquals("line 3", FileUtils.readLineAt(2, f2));
-        assertEquals("line 3", FileUtils.readLineAt(2, f3));
-        assertEquals("line 4", FileUtils.readLineAt(3, f1));
-        assertEquals("line 4", FileUtils.readLineAt(3, f2));
-        assertEquals("line 4", FileUtils.readLineAt(3, f3));
-    }
-
-    @Test
-    public void readTest() throws IOException {
-        //read whole content
-        String data = FileUtils.readFile("src/test/resources/fileUtils.txt");
-        String expected = "a\nab\nabc\n\n\n\n\n\n\n\n\\n\n\n\n\n\ndfe\n\ntest";
-        assertEquals(expected, data);
-    }
-
-    @Test
-    public void getHashTest(){
-        //check if hash abs works
-        assertTrue(FileUtils.getHashcodeFromFileContent("src/test/resources/fileUtils.txt")>0);
+    public static class NonParameterizedTest {
+        @Test
+        public void readTest() throws IOException {
+            //read whole content
+            String data = FileUtils.readFile("src/test/resources/fileUtils.txt");
+            String expected = "a\nab\nabc\n\n\n\n\n\n\n\n\\n\n\n\n\n\ndfe\n\ntest";
+            assertEquals(expected, data);
+        }
     }
 }
