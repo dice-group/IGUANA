b50fd1c66afa060ae829e734600518a4bde7e133
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-10-06 17:49:12 +0200

    update QueryHandlerConfigTest

--- src/test/java/org/aksw/iguana/cc/query/handler/QueryHandlerConfigTest.java
+++ src/test/java/org/aksw/iguana/cc/query/handler/QueryHandlerConfigTest.java
@@ -2,6 +2,8 @@ package org.aksw.iguana.cc.query.handler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,12 +23,24 @@ class QueryHandlerConfigTest {
                                 QueryHandler.Config.Format.FOLDER,
                                 true,
                                 QueryHandler.Config.Order.LINEAR,
-                                null,
+                                100L,
                                 QueryHandler.Config.Language.SPARQL
                         ),
                         """
-                                {"path":"some.queries","format":"folder","caching":true,"order":"linear","lang":"SPARQL"}
-                                """),
+                            {"path":"some.queries","format":"folder","caching":true,"order":"linear","seed": 100, "lang":"SPARQL"}
+                        """
+                ),
+                Arguments.of(new QueryHandler.Config("some.queries",
+                                QueryHandler.Config.Format.ONE_PER_LINE,
+                                true,
+                                QueryHandler.Config.Order.LINEAR,
+                                0L,
+                                QueryHandler.Config.Language.SPARQL
+                        ),
+                        """
+                            {"path":"some.queries"}
+                        """
+                ),
                 Arguments.of(new QueryHandler.Config("some.queries",
                                 QueryHandler.Config.Format.FOLDER,
                                 true,
@@ -35,13 +49,16 @@ class QueryHandlerConfigTest {
                                 QueryHandler.Config.Language.SPARQL
                         ),
                         """
-                                {"path":"some.queries","format":"folder","caching":true,"order":"random","seed":42,"lang":"SPARQL"}
-                                """));
+                            {"path":"some.queries","format":"folder","caching":true,"order":"random","seed":42,"lang":"SPARQL"}
+                        """
+                )
+        );
 
     }
 
     @ParameterizedTest
     @MethodSource("testData")
+    @Disabled("Serialization isn't used")
     public void testSerialization(QueryHandler.Config config, String expectedJson) throws Exception {
 
         final String actual = mapper.writeValueAsString(config);
