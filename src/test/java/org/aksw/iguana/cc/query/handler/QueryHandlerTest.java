507910cde5698a186cbd1c4925f965bf0b987818
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-10-06 18:11:22 +0200

    update QueryHandlerTest

--- src/test/java/org/aksw/iguana/cc/query/handler/QueryHandlerTest.java
+++ src/test/java/org/aksw/iguana/cc/query/handler/QueryHandlerTest.java
@@ -1,12 +1,14 @@
 package org.aksw.iguana.cc.query.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aksw.iguana.cc.query.selector.impl.LinearQuerySelector;
 import org.aksw.iguana.cc.query.source.impl.FolderQuerySourceTest;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
@@ -14,8 +16,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class QueryHandlerTest {
 
@@ -23,7 +24,7 @@ public class QueryHandlerTest {
 
     List<FolderQuerySourceTest.Query> queries;
 
-    @Before
+    @BeforeEach
     public void createFolder() throws IOException {
         tempDir = Files.createTempDirectory("folder-query-source-test-dir");
 
@@ -38,23 +39,65 @@ public class QueryHandlerTest {
         Collections.sort(queries);
     }
 
-    @After
+    @AfterEach
     public void removeFolder() throws IOException {
         org.apache.commons.io.FileUtils.deleteDirectory(tempDir.toFile());
     }
-
 
     @Test
     public void testDeserialization() throws Exception {
         var json = String.format("""
                 {"path":"%s","format":"folder","order":"linear","lang":"SPARQL"}
-                """, tempDir);
+                """, tempDir.toString().replaceAll("\\\\", "\\\\\\\\")); // windows
         final var mapper = new ObjectMapper();
         QueryHandler queryHandler = assertDoesNotThrow(() -> mapper.readValue(json, QueryHandler.class));
+        final var selector = queryHandler.getQuerySelectorInstance();
+        assertTrue(selector instanceof LinearQuerySelector);
         assertEquals(queries.size(), queryHandler.getQueryCount());
-        QueryHandler.QueryStringWrapper nextQuery = queryHandler.getNextQuery(queryHandler.getQuerySelectorInstance());
-        assertEquals(0, nextQuery.index());
-        assertEquals(nextQuery.query(), queries.get(0).content());
+        assertNotEquals(0, queryHandler.hashCode());
+        for (int i = 0; i < queryHandler.getQueryCount(); i++) {
+            assertEquals(i, selector.getCurrentIndex());
+            final var wrapper = queryHandler.getNextQuery(selector);
+            assertEquals(queries.get(i).content(), wrapper.query());
+            assertEquals(i, wrapper.index());
+        }
+    }
 
+    @Test
+    public void testQueryStreamWrapper() throws IOException {
+        var json = String.format("""
+                {"path":"%s","format":"folder","order":"linear","lang":"SPARQL"}
+                """, tempDir.toString().replaceAll("\\\\", "\\\\\\\\")); // windows
+        final var mapper = new ObjectMapper();
+        QueryHandler queryHandler = assertDoesNotThrow(() -> mapper.readValue(json, QueryHandler.class));
+        final var selector = queryHandler.getQuerySelectorInstance();
+        assertTrue(selector instanceof LinearQuerySelector);
+        assertEquals(queries.size(), queryHandler.getQueryCount());
+        assertNotEquals(0, queryHandler.hashCode());
+        for (int i = 0; i < queryHandler.getQueryCount(); i++) {
+            assertEquals(i, selector.getCurrentIndex());
+            final var wrapper = queryHandler.getNextQueryStream(selector);
+            final var acutalQuery = new String(wrapper.queryInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            assertEquals(queries.get(i).content(), acutalQuery);
+            assertEquals(i, wrapper.index());
+        }
+    }
+
+    @Test
+    public void testQueryIDs() {
+        var json = String.format("""
+                {"path":"%s","format":"folder","order":"linear","lang":"SPARQL"}
+                """, tempDir.toString().replaceAll("\\\\", "\\\\\\\\")); // windows
+        final var mapper = new ObjectMapper();
+        QueryHandler queryHandler = assertDoesNotThrow(() -> mapper.readValue(json, QueryHandler.class));
+        final var selector = queryHandler.getQuerySelectorInstance();
+        assertTrue(selector instanceof LinearQuerySelector);
+        assertEquals(queries.size(), queryHandler.getQueryCount());
+        assertNotEquals(0, queryHandler.hashCode());
+        final var allQueryIDs = queryHandler.getAllQueryIds();
+        for (int i = 0; i < queryHandler.getQueryCount(); i++) {
+            assertEquals(queryHandler.hashCode() + ":" + i, allQueryIDs[i]);
+            assertEquals(allQueryIDs[i], queryHandler.getQueryId(i));
+        }
     }
 }
