26563a69668427eaa8e7f7d837a65c0d2435cbdc
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-10-12 14:16:06 +0200

    parameterize RandomQuerySelectorTest

--- src/test/java/org/aksw/iguana/cc/query/selector/impl/RandomQuerySelectorTest.java
+++ src/test/java/org/aksw/iguana/cc/query/selector/impl/RandomQuerySelectorTest.java
@@ -1,6 +1,8 @@
 package org.aksw.iguana.cc.query.selector.impl;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,16 +18,18 @@ public class RandomQuerySelectorTest {
         }
     }
 
-    @Test
-    public void testThrowingOnIllegalSize() {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    public void testThrowingOnIllegalSize(int size) {
         assertThrows(IllegalArgumentException.class, () -> new RandomQuerySelector(-1, 0));
         assertThrows(IllegalArgumentException.class, () -> new RandomQuerySelector(0, 0));
     }
 
-    @Test
-    public void testSeedConsistency() {
-        final var selector = new RandomQuerySelector(100000, 0);
-        final var selector2 = new RandomQuerySelector(100000, 0);
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 100000})
+    public void testSeedConsistency(int size) {
+        final var selector = new RandomQuerySelector(size, 0);
+        final var selector2 = new RandomQuerySelector(size, 0);
         for (int i = 0; i < 100000; i++) {
             final var nextIndex = selector.getNextIndex();
             final var nextIndex2 = selector2.getNextIndex();
