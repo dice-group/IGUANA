cb7de4a03a40213edb5010fa3becc4e4662cd5e6
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-10-24 11:15:09 +0200

    update query source tests

--- src/test/java/org/aksw/iguana/cc/query/source/impl/FileLineQuerySourceTest.java
+++ src/test/java/org/aksw/iguana/cc/query/source/impl/FileLineQuerySourceTest.java
@@ -1,51 +1,114 @@
 package org.aksw.iguana.cc.query.source.impl;
 
-import org.aksw.iguana.cc.utils.FileUtils;
-import org.junit.Test;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.text.StringEscapeUtils;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.IntStream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class FileLineQuerySourceTest {
+    private record SourceConfig(int size, String lineEnding, boolean overshoot) {
+        @Override
+        public String toString() {
+            return "{ size: " + size + ", overshoot: " + overshoot + ", line_ending: " + StringEscapeUtils.escapeJava(lineEnding) + " }";
+        }
+    }
 
-    private static final Path PATH = Paths.get("src/test/resources/query/source/queries.txt");
+    private final static Function<Integer, String> queryTemplate = (i) -> "Query " + i + " {still query " + i + "}";
 
-    private final FileLineQuerySource querySource;
+    private static Path directory;
+    private static List<Arguments> cachedArguments = null;
 
-    public FileLineQuerySourceTest() throws IOException {
-        querySource = new FileLineQuerySource(PATH);
+    private static String createFileContent(int size, String lineEnding, boolean overshoot) {
+        final var stringBuilder = new StringBuilder();
+        int limit = overshoot ? size : size - 1;
+        for (int i = 0; i < limit; i++) {
+            stringBuilder.append(queryTemplate.apply(i)).append(lineEnding);
+        }
+        if (!overshoot) {
+            stringBuilder.append(queryTemplate.apply(size - 1));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static List<Arguments> createTestSource() throws IOException {
+        if (cachedArguments != null) {
+            return cachedArguments;
+        }
+        List<Arguments> output = new ArrayList<>();
+        int[] sizes = { 1, 1000 };
+        String[] lineEndings = { "\n", "\r\n", "\r" };
+        boolean[] overshoots = { false, true };
+        for (int size : sizes) {
+            for (String lineEnding : lineEndings) {
+                for (boolean overshoot : overshoots) {
+                    final var fileContent = createFileContent(size, lineEnding, overshoot);
+                    final var filePath = Files.createTempFile(directory, "Query", ".txt");
+                    Files.writeString(filePath, fileContent);
+                    final var querySource = new FileLineQuerySource(filePath);
+                    output.add(Arguments.of(querySource, new SourceConfig(size, lineEnding, overshoot)));
+                }
+            }
+        }
+        cachedArguments = output;
+        return output;
+    }
+
+    @BeforeAll
+    public static void createTempDirectory() throws IOException {
+        directory = Files.createTempDirectory("file-line-query-source-test-dir");
+    }
+
+    @AfterAll
+    public static void deleteTempDirectory() throws IOException {
+        FileUtils.deleteDirectory(directory.toFile());
     }
 
     @Test
-    public void sizeTest() {
-        assertEquals(3, this.querySource.size());
+    public void testInitialization() throws IOException {
+        assertThrows(NullPointerException.class, () -> new FileLineQuerySource(null));
+        assertDoesNotThrow(() -> new FileLineQuerySource(Files.createTempFile(directory, "Query", ".txt")));
+        final var notEmptyFile = Files.createTempFile(directory, "Query", ".txt");
+        Files.writeString(notEmptyFile, "not empty");
+        assertDoesNotThrow(() -> new FileLineQuerySource(notEmptyFile));
     }
 
-    @Test
-    public void getQueryTest() throws IOException {
-        assertEquals("QUERY 1 {still query 1}", this.querySource.getQuery(0));
-        assertEquals("QUERY 2 {still query 2}", this.querySource.getQuery(1));
-        assertEquals("QUERY 3 {still query 3}", this.querySource.getQuery(2));
+    @ParameterizedTest(name = "[{index}] config = {1}")
+    @MethodSource("createTestSource")
+    public void sizeTest(FileLineQuerySource querySource, SourceConfig config) throws IOException {
+        assertEquals(config.size, querySource.size());
     }
 
-    @Test
-    public void getAllQueriesTest() throws IOException {
-        List<String> expected = new ArrayList<>(3);
-        expected.add("QUERY 1 {still query 1}");
-        expected.add("QUERY 2 {still query 2}");
-        expected.add("QUERY 3 {still query 3}");
-
-        assertEquals(expected, this.querySource.getAllQueries());
+    @ParameterizedTest(name = "[{index}] config = {1}")
+    @MethodSource("createTestSource")
+    public void getQueryTest(FileLineQuerySource querySource, SourceConfig config) throws IOException {
+        for (int i = 0; i < config.size; i++) {
+            assertEquals(queryTemplate.apply(i), querySource.getQuery(i));
+        }
     }
 
-    @Test
-    public void getHashcodeTest() {
-        int expected = FileUtils.getHashcodeFromFileContent(PATH);
-        assertEquals(expected, this.querySource.hashCode());
+    @ParameterizedTest(name = "[{index}] config = {1}")
+    @MethodSource("createTestSource")
+    public void getAllQueriesTest(FileLineQuerySource querySource, SourceConfig config) throws IOException {
+        List<String> expected = IntStream.range(0, config.size).mapToObj(i -> queryTemplate.apply(i)).toList();
+        assertEquals(expected, querySource.getAllQueries());
+    }
+
+    @ParameterizedTest(name = "[{index}] config = {1}")
+    @MethodSource("createTestSource")
+    public void getHashcodeTest(FileLineQuerySource querySource, SourceConfig config) {
+        assertTrue(querySource.hashCode() != 0);
     }
 }
--- src/test/java/org/aksw/iguana/cc/query/source/impl/FolderQuerySourceTest.java
+++ src/test/java/org/aksw/iguana/cc/query/source/impl/FolderQuerySourceTest.java
@@ -1,8 +1,10 @@
 package org.aksw.iguana.cc.query.source.impl;
 
-import org.junit.*;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,26 +13,10 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Parameterized.class)
 public class FolderQuerySourceTest {
-
-    Path tempDir;
-    TestConfig testConfig;
-
-    public FolderQuerySourceTest(TestConfig testConfig) {
-        this.testConfig = testConfig;
-    }
-
-    public static class TestConfig {
-
-        public TestConfig(int numberOfQueries) {
-            this.numberOfQueries = numberOfQueries;
-        }
-
-        int numberOfQueries;
-    }
+    static Path tempDir;
 
     public record Query(Path queryFile, String content) implements Comparable<Query> {
 
@@ -40,48 +26,47 @@ public class FolderQuerySourceTest {
         }
     }
 
-
-    List<Query> queries;
-
-
-    @Parameterized.Parameters
-    public static Collection<TestConfig> data() {
-        return List.of(new TestConfig(0),
-                new TestConfig(1),
-                new TestConfig(2),
-                new TestConfig(5));
-    }
-
-    @Before
-    public void createFolder() throws IOException {
-        this.tempDir = Files.createTempDirectory("folder-query-source-test-dir");
-
-        this.queries = new LinkedList<>();
-        for (int i = 0; i < testConfig.numberOfQueries; i++) {
-            final Path queryFile = Files.createTempFile(tempDir, "Query", ".txt");
-            final String content = UUID.randomUUID().toString();
-            Files.write(queryFile, content.getBytes(StandardCharsets.UTF_8));
-            this.queries.add(new Query(queryFile, content));
+    public static List<Arguments> data() throws IOException {
+        final var sizes = List.of(1, 2, 10, 100, 1000);
+        final var out = new ArrayList<Arguments>();
+        for (int size : sizes) {
+            final var queries = new LinkedList<Query>();
+            final var queryDir = Files.createTempDirectory(tempDir, "query-dir");
+            for (int i = 0; i < size; i++) {
+                final Path queryFile = Files.createTempFile(queryDir, "Query", ".txt");
+                final String content = UUID.randomUUID().toString();
+                Files.write(queryFile, content.getBytes(StandardCharsets.UTF_8));
+                queries.add(new Query(queryFile, content));
+            }
+            // Queries in the folder are expected in alphabetic order of the file names.
+            Collections.sort(queries);
+            out.add(Arguments.of(queryDir, queries));
         }
-        // Queries in the folder are expected in alphabetic order of the file names.
-        Collections.sort(this.queries);
+        return out;
     }
 
-    @After
-    public void removeFolder() throws IOException {
-        org.apache.commons.io.FileUtils.deleteDirectory(this.tempDir.toFile());
+    @BeforeAll
+    public static void createFolder() throws IOException {
+        tempDir = Files.createTempDirectory("folder-query-source-test-dir");
     }
 
-    @Test
-    public void testFolderQuerySource() throws IOException {
+
+    @AfterAll
+    public static void removeFolder() throws IOException {
+        org.apache.commons.io.FileUtils.deleteDirectory(tempDir.toFile());
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testFolderQuerySource(Path tempDir, List<Query> expectedQueries) throws IOException {
         FolderQuerySource querySource = new FolderQuerySource(tempDir);
 
-        assertEquals(this.queries.size(), querySource.size());
+        assertEquals(expectedQueries.size(), querySource.size());
 
         for (int i = 0; i < querySource.size(); i++) {
-            assertEquals(queries.get(i).content, querySource.getQuery(i));
+            assertEquals(expectedQueries.get(i).content, querySource.getQuery(i));
         }
 
-        assertEquals(queries.stream().map(q -> q.content).collect(Collectors.toList()), querySource.getAllQueries());
+        assertEquals(expectedQueries.stream().map(q -> q.content).collect(Collectors.toList()), querySource.getAllQueries());
     }
 }
