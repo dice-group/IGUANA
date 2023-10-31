1cc4409e79032acf2dcf978d3b3fe2ccaa3aaacd
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-10-31 17:46:40 +0100

    migrate the IndexedQueryReaderTest to JUnit 5

--- src/test/java/org/aksw/iguana/cc/utils/IndexedQueryReaderTest.java
+++ src/test/java/org/aksw/iguana/cc/utils/IndexedQueryReaderTest.java
@@ -1,101 +1,65 @@
 package org.aksw.iguana.cc.utils;
 
-import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Enclosed.class)
 public class IndexedQueryReaderTest {
 
-    @RunWith(Parameterized.class)
-    public static class ParameterizedTest {
-
-        IndexedQueryReader reader;
-
-        @Parameterized.Parameters
-        public static Collection<Object[]> data() {
-            return Arrays.asList(new Object[][]{
-                    {"src/test/resources/readLineTestFile1.txt"},
-                    {"src/test/resources/readLineTestFile2.txt"},
-                    {"src/test/resources/readLineTestFile3.txt"}
-            });
-        }
-
-        public ParameterizedTest(String path) throws IOException {
-            reader = IndexedQueryReader.make(Paths.get(path));
-        }
-
-        @Test
-        public void testIndexingWithLineEndings() throws IOException {
-            assertEquals("line 1", reader.readQuery(0));
-            assertEquals("line 2", reader.readQuery(1));
-            assertEquals("line 3", reader.readQuery(2));
-            assertEquals("line 4", reader.readQuery(3));
-        }
+    @org.junit.jupiter.params.ParameterizedTest
+    @ValueSource(strings = {"src/test/resources/readLineTestFile1.txt", "src/test/resources/readLineTestFile2.txt", "src/test/resources/readLineTestFile3.txt"})
+    public void testIndexingWithLineEndings(String path) throws IOException {
+        var reader = IndexedQueryReader.make(Paths.get(path));
+        assertEquals("line 1", reader.readQuery(0));
+        assertEquals("line 2", reader.readQuery(1));
+        assertEquals("line 3", reader.readQuery(2));
+        assertEquals("line 4", reader.readQuery(3));
     }
 
-    public static class NonParameterizedTest {
-        @Test
-        public void testIndexingWithBlankLines() throws IOException {
-            IndexedQueryReader reader = IndexedQueryReader.makeWithEmptyLines(Paths.get("src/test/resources/utils/indexingtestfile3.txt"));
-            String le = FileUtils.getLineEnding(Paths.get("src/test/resources/utils/indexingtestfile3.txt"));
+    @Test
+    public void testIndexingWithBlankLines() throws IOException {
+        IndexedQueryReader reader = IndexedQueryReader.makeWithEmptyLines(Paths.get("src/test/resources/utils/indexingtestfile3.txt"));
+        String le = FileUtils.getLineEnding(Paths.get("src/test/resources/utils/indexingtestfile3.txt"));
 
-            assertEquals(" line 1" + le + "line 2", reader.readQuery(0));
-            assertEquals("line 3", reader.readQuery(1));
-            assertEquals("line 4" + le + "line 5", reader.readQuery(2));
-        }
+        assertEquals(" line 1" + le + "line 2", reader.readQuery(0));
+        assertEquals("line 3", reader.readQuery(1));
+        assertEquals("line 4" + le + "line 5", reader.readQuery(2));
     }
 
-    @RunWith(Parameterized.class)
-    public static class TestCustomSeparator {
-        private static class TestData {
-            public Path filepath;
-            public String separator;
-            public String[] expectedStrings;
+    private record TestData (
+        Path filepath,
+        String separator,
+        String[] expectedStrings
+    ) {}
 
-            public TestData(String filepath, String separator, String[] expectedStrings) {
-                this.filepath = Path.of(filepath);
-                this.separator = separator;
-                this.expectedStrings = expectedStrings;
-            }
+    public static Collection<TestData> data() throws IOException {
+        // all the files should have the same line ending
+        String le = FileUtils.getLineEnding(Path.of("src/test/resources/utils/indexingtestfile1.txt"));
+        return List.of(
+                new TestData(Path.of("src/test/resources/utils/indexingtestfile1.txt"), "#####" + le, new String[]{"line 1" + le, le + "line 2" + le}),
+                new TestData(Path.of("src/test/resources/utils/indexingtestfile2.txt"), "#####" + le, new String[]{"line 0" + le, "line 1" + le + "#####"}),
+                new TestData(Path.of("src/test/resources/utils/indexingtestfile4.txt"), "###$", new String[]{"a#", "b"}),
+                new TestData(Path.of("src/test/resources/utils/indexingtestfile5.txt"), "211", new String[]{"a21", "b"})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    public void testIndexingWithCustomSeparator(TestData data) throws IOException {
+        IndexedQueryReader reader = IndexedQueryReader.makeWithStringSeparator(data.filepath, data.separator);
+        for (int i = 0; i < data.expectedStrings.length; i++) {
+            String read = reader.readQuery(i);
+            assertEquals(data.expectedStrings[i], read);
         }
-
-        private TestData data;
-
-        public TestCustomSeparator(TestData data) {
-            this.data = data;
-        }
-
-        @Parameterized.Parameters
-        public static Collection<TestData> data() throws IOException {
-            // all the files should have the same line ending
-            String le = FileUtils.getLineEnding(Path.of("src/test/resources/utils/indexingtestfile1.txt"));
-            return List.of(
-                    new TestData("src/test/resources/utils/indexingtestfile1.txt", "#####" + le, new String[]{"line 1" + le, le + "line 2" + le}),
-                    new TestData("src/test/resources/utils/indexingtestfile2.txt", "#####" + le, new String[]{"line 0" + le, "line 1" + le + "#####"}),
-                    new TestData("src/test/resources/utils/indexingtestfile4.txt", "###$", new String[]{"a#", "b"}),
-                    new TestData("src/test/resources/utils/indexingtestfile5.txt", "211", new String[]{"a21", "b"})
-            );
-        }
-
-        @Test
-        public void testIndexingWithCustomSeparator() throws IOException {
-            IndexedQueryReader reader = IndexedQueryReader.makeWithStringSeparator(this.data.filepath, this.data.separator);
-            for (int i = 0; i < this.data.expectedStrings.length; i++) {
-                String read = reader.readQuery(i);
-                assertEquals(this.data.expectedStrings[i], read);
-            }
-            assertEquals(this.data.expectedStrings.length, reader.readQueries().size());
-        }
+        assertEquals(data.expectedStrings.length, reader.readQueries().size());
     }
 }
