c2d215eebbbef7d2eb4a0b7bd4349597416c8aa9
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2023-11-03 19:51:52 +0100

    Update TriplestoreStorageTest

--- src/test/java/org/aksw/iguana/cc/storage/impl/TriplestoreStorageTest.java
+++ src/test/java/org/aksw/iguana/cc/storage/impl/TriplestoreStorageTest.java
@@ -1,79 +1,70 @@
 package org.aksw.iguana.cc.storage.impl;
-import org.aksw.iguana.cc.tasks.ServerMock;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Disabled;
+
+import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
+import com.github.tomakehurst.wiremock.core.Options;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
+import org.aksw.iguana.cc.mockup.MockupQueryHandler;
+import org.aksw.iguana.cc.mockup.MockupWorker;
+import org.aksw.iguana.cc.worker.HttpWorker;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateRequest;
 import org.junit.jupiter.api.Test;
-import org.simpleframework.http.core.ContainerServer;
-import org.simpleframework.transport.connect.SocketConnection;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Will test if the TriplestoreStorage sends the correct INSERT command to a Mock Server
- * 
- * @author f.conrads
- *
- */
-@Disabled("TriplestoreStorage hasn't been looked at in a while and the test is outdated.")
-public class TriplestoreStorageTest {
+public class TriplestoreStorageTest extends StorageTest {
 
-    private static final int FAST_SERVER_PORT = 8023;
-	private ServerMock fastServerContainer;
-	private ContainerServer fastServer;
-	private static SocketConnection fastConnection;
+	@RegisterExtension
+	public static WireMockExtension wm = WireMockExtension.newInstance()
+			.options(new WireMockConfiguration().useChunkedTransferEncoding(Options.ChunkedEncodingPolicy.NEVER).dynamicPort().notifier(new Slf4jNotifier(true)))
+			.failOnUnmatchedRequests(true)
+			.build();
 
-	private String metaExp = "INSERT DATA {\n" +
-			"  <http://iguana-benchmark.eu/resource/1/1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iguana-benchmark.eu/class/Experiment> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1/1> <http://iguana-benchmark.eu/properties/dataset> <http://iguana-benchmark.eu/resource/dbpedia> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1/1> <http://iguana-benchmark.eu/properties/task> <http://iguana-benchmark.eu/resource/1/1/1> .\n" +
-			"  <http://iguana-benchmark.eu/resource/dbpedia> <http://www.w3.org/2000/01/rdf-schema#label> \"dbpedia\" .\n" +
-			"  <http://iguana-benchmark.eu/resource/dbpedia> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iguana-benchmark.eu/class/Dataset> .\n" +
-			"  <http://iguana-benchmark.eu/resource/virtuoso> <http://www.w3.org/2000/01/rdf-schema#label> \"virtuoso\" .\n" +
-			"  <http://iguana-benchmark.eu/resource/virtuoso> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iguana-benchmark.eu/class/Connection> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1/1/1> <http://www.w3.org/2000/01/rdf-schema#startDate> \"???\"^^<http://www.w3.org/2001/XMLSchema#dateTime> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1/1/1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iguana-benchmark.eu/resource/ClassName> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1/1/1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iguana-benchmark.eu/class/Task> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1/1/1> <http://iguana-benchmark.eu/properties/connection> <http://iguana-benchmark.eu/resource/virtuoso> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://iguana-benchmark.eu/class/Suite> .\n" +
-			"  <http://iguana-benchmark.eu/resource/1> <http://iguana-benchmark.eu/properties/experiment> <http://iguana-benchmark.eu/resource/1/1> .\n" +
-			"}";
-
-	private String dataExp = "INSERT DATA {\n"+
-"  <http://iguana-benchmark.eu/resource/a> <http://iguana-benchmark.eu/properties/b> \"c\" .\n"+
-"}";
-
-	/**
-	 * @throws IOException
-	 */
-	@AfterAll
-	public static void close() throws IOException {
-		fastConnection.close();
-	}
-
-	/**
-	 * @throws IOException
-	 */
 	@Test
-	public void dataTest() throws IOException {
-		fastServerContainer = new ServerMock();
-        fastServer = new ContainerServer(fastServerContainer);
-        fastConnection = new SocketConnection(fastServer);
-        SocketAddress address1 = new InetSocketAddress(FAST_SERVER_PORT);
-        fastConnection.connect(address1);
-        
-        String host = "http://localhost:8023";
-        TriplestoreStorage store = new TriplestoreStorage(host);
+	public void dataTest() throws URISyntaxException {
+		final var uuid = UUID.randomUUID();
+		wm.stubFor(post(urlEqualTo("/ds/sparql"))
+						.willReturn(aResponse()
+								.withStatus(200)))
+				.setId(uuid);
 
-	    Model m = ModelFactory.createDefaultModel();
-	    // m.add(ResourceFactory.createResource(COMMON.RES_BASE_URI+"a"), ResourceFactory.createProperty(COMMON.PROP_BASE_URI+"b") , "c");
-	    store.storeResult(m);
-        assertEquals(dataExp.trim(),fastServerContainer.getActualContent().trim());
+		final List<List<HttpWorker>> worker = List.of(List.of(
+				new MockupWorker(
+						0,
+						new MockupQueryHandler(1, 2),
+						"conneciton",
+						"v2",
+						"wikidata",
+						Duration.ofSeconds(2))
+		));
+		final var testData = createTaskResult(worker, 0, "1");
+
+		final var uri = new URI("http://localhost:" + wm.getPort() + "/ds/sparql");
+		final var storage = new TriplestoreStorage(String.valueOf(uri));
+		storage.storeResult(testData.resultModel());
+
+		List<ServeEvent> allServeEvents = wm.getAllServeEvents();
+		ServeEvent request = allServeEvents.get(0);
+		String body = request.getRequest().getBodyAsString();
+
+		StringWriter nt = new StringWriter();
+		RDFDataMgr.write(nt, testData.resultModel(), org.apache.jena.riot.Lang.NTRIPLES);
+
+		UpdateRequest updateRequestActual = UpdateFactory.create(body);
+		UpdateRequest updateRequestExpected = UpdateFactory.create().add("INSERT DATA { " + nt + " }");
+
+		assertTrue(updateRequestExpected.iterator().next().equalTo(updateRequestActual.iterator().next(), null));
 	}
-
 }
