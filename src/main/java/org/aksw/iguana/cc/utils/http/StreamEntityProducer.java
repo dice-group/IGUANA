d517e9cbc577a0ebb78457e11a43b9f09e4430e7
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2024-02-16 12:47:43 +0100

    StreamEntityProducer can send fixed-sized data and is reproducible now

--- src/main/java/org/aksw/iguana/cc/utils/http/StreamEntityProducer.java
+++ src/main/java/org/aksw/iguana/cc/utils/http/StreamEntityProducer.java
@@ -1,63 +1,136 @@
 package org.aksw.iguana.cc.utils.http;
 
-import org.apache.hc.core5.http.ContentType;
-import org.apache.hc.core5.http.nio.StreamChannel;
-import org.apache.hc.core5.http.nio.entity.AbstractBinAsyncEntityProducer;
+import org.apache.hc.core5.http.nio.AsyncEntityProducer;
+import org.apache.hc.core5.http.nio.DataStreamChannel;
+import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.Set;
+import java.util.function.Supplier;
 
-public class StreamEntityProducer extends AbstractBinAsyncEntityProducer {
+public class StreamEntityProducer implements AsyncEntityProducer {
 
-    private final byte[] buffer = new byte[8192];
-    private final InputStream stream;
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(StreamEntityProducer.class);
 
-    public StreamEntityProducer(InputStream stream, ContentType contentType) {
-        super(8192, contentType);
-        this.stream = stream;
-    }
+    private final Supplier<InputStream> streamSupplier;
+    private final boolean chunked;
 
-    @Override
-    protected int availableData() {
-        try {
-            return stream.available();
-        } catch (IOException ignore) {} // TODO: log
-        return 0;
-    }
+    private ByteBuffer content;
 
-    @Override
-    protected void produceData(StreamChannel<ByteBuffer> channel) throws IOException {
-        int read;
-        if ((read = stream.read(buffer)) != -1) {
-            channel.write(ByteBuffer.wrap(buffer, 0, read));
-        } else {
-            channel.endStream();
+    private final static int BUFFER_SIZE = 8192;
+    private final byte[] buffer = new byte[BUFFER_SIZE];
+    
+    private InputStream currentStream;
+
+    public StreamEntityProducer(Supplier<InputStream> streamSupplier, boolean chunked) throws IOException {
+        this.streamSupplier = streamSupplier;
+        this.chunked = chunked;
+
+        if (!chunked) {
+            content = ByteBuffer.wrap(streamSupplier.get().readAllBytes());
         }
     }
 
     @Override
     public boolean isRepeatable() {
-        return false;
-    }
-
-    @Override
-    public void failed(Exception cause) {
-        // TODO: log
-    }
-
-    @Override
-    public boolean isChunked() {
         return true;
     }
 
     @Override
+    public void failed(Exception cause) {
+        logger.error("Failed to produce entity data", cause);
+        if (currentStream != null) {
+            try {
+                currentStream.close();
+            } catch (IOException e) {
+                logger.error("Failed to close input stream", e);
+            }
+        }
+    }
+
+    @Override
+    public boolean isChunked() {
+        return chunked;
+    }
+
+    @Override
+    public Set<String> getTrailerNames() {
+        return null;
+    }
+
+    @Override
     public long getContentLength() {
-        return super.getContentLength();
+        // if the content length is known (non-chunked request), return it
+        if (content != null) {
+            return content.limit();
+        }
+
+        // if the content length is unknown (chunked request), return -1
+        return -1;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public String getContentEncoding() {
+        return null;
     }
 
     @Override
     public void releaseResources() {
-        super.releaseResources();
+        if (currentStream != null) {
+            try {
+                currentStream.close();
+            } catch (IOException e) {
+                logger.error("Failed to close input stream", e);
+            }
+        }
+    }
+
+    @Override
+    public int available() {
+        if (content != null) {
+            return content.remaining();
+        }
+        if (currentStream != null) {
+            try {
+                return currentStream.available();
+            } catch (IOException e) {
+                logger.error("Failed to get available bytes from input stream", e);
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void produce(DataStreamChannel channel) throws IOException {
+        // handling of non-chunked request
+        if (content != null) {
+            channel.write(content);
+            if (!content.hasRemaining()) {
+                channel.endStream();
+            }
+            return;
+        }
+
+        // handling of chunked request
+        if (chunked && currentStream == null) {
+            currentStream = streamSupplier.get();
+        }
+
+        int bytesRead;
+        while ((bytesRead = currentStream.read(buffer)) > 0) {
+            ByteBuffer byteBuffer = ByteBuffer.wrap(buffer, 0, bytesRead);
+            channel.write(byteBuffer);
+        }
+
+        if (bytesRead == -1) {
+            channel.endStream();
+        }
     }
 }
