e651e8dd45f5791f90e1617e2f41dfae0dabda9c
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2024-09-13 16:53:49 +0200

    Add hash to instance file

--- src/main/java/org/aksw/iguana/cc/query/handler/QueryHandler.java
+++ src/main/java/org/aksw/iguana/cc/query/handler/QueryHandler.java
@@ -189,9 +189,11 @@ public class QueryHandler {
     private QueryList initializeTemplateQueryHandler(QuerySource templateSource) throws IOException {
         QuerySource querySource = templateSource;
         final var originalPath = templateSource.getPath();
+        final var postfix = String.format("_instances_%s.txt",
+                Integer.toUnsignedString(this.config.template.endpoint.hashCode() + (int) this.config.template.limit.longValue()));
         final Path instancePath = Files.isDirectory(originalPath) ?
-                originalPath.resolveSibling(originalPath.getFileName() + "_instances.txt") : // if the source of the query templates is a folder, the instances will be saved in a file with the same name as the folder
-                originalPath.resolveSibling(originalPath.getFileName().toString().split("\\.")[0] + "_instances.txt"); // if the source of the query templates is a file, the instances will be saved in a file with the same name as the file
+                originalPath.resolveSibling(originalPath.getFileName() + postfix) : // if the source of the query templates is a folder, the instances will be saved in a file with the same name as the folder
+                originalPath.resolveSibling(originalPath.getFileName().toString().split("\\.")[0] + postfix); // if the source of the query templates is a file, the instances will be saved in a file with the same name as the file
         if (Files.exists(instancePath)) {
             LOGGER.info("Already existing query template instances have been found and will be reused. Delete the following file to regenerate them: {}", instancePath.toAbsolutePath());
             querySource = createQuerySource(instancePath); // if the instances already exist, use them
