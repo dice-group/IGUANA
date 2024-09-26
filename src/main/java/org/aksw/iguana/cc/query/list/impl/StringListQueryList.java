3aa46ed31cbc769d6b1ee090383d7692211b44ed
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2024-09-26 15:16:05 +0200

    Fix StringListQueryList

--- src/main/java/org/aksw/iguana/cc/query/list/impl/StringListQueryList.java
+++ src/main/java/org/aksw/iguana/cc/query/list/impl/StringListQueryList.java
@@ -1,9 +1,8 @@
 package org.aksw.iguana.cc.query.list.impl;
 
-import org.aksw.iguana.cc.query.QueryData;
 import org.aksw.iguana.cc.query.list.QueryList;
+import org.aksw.iguana.commons.io.ByteArrayListInputStream;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -23,7 +22,7 @@ public class StringListQueryList implements QueryList {
 
     @Override
     public InputStream getQueryStream(int index) throws IOException {
-        return new ByteArrayInputStream(queries.get(index).getBytes());
+        return new ByteArrayListInputStream(List.of(queries.get(index).getBytes()));
     }
 
     @Override
