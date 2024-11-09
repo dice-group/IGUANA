e967ff25f8e3e6675642acb68ae48d1e819d37c4
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2024-11-09 13:49:22 +0100

    Remove unused import

--- src/main/java/org/aksw/iguana/cc/query/handler/TemplateHandler.java
+++ src/main/java/org/aksw/iguana/cc/query/handler/TemplateHandler.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
