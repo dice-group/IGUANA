2e5e430a2ba3eec33961b34a61befa0ae2546665
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2025-07-28 14:15:08 +0200

    Make QPS and QMPH metric consider timed out queries

--- src/main/java/org/aksw/iguana/cc/metrics/impl/QMPH.java
+++ src/main/java/org/aksw/iguana/cc/metrics/impl/QMPH.java
@@ -27,13 +27,13 @@ public class QMPH extends Metric implements TaskMetric, WorkerMetric {
 
     @Override
     public Number calculateWorkerMetric(HttpWorker.Config worker, List<HttpWorker.ExecutionStats>[] data) {
-        BigDecimal successes = BigDecimal.ZERO;
+        BigDecimal executions = BigDecimal.ZERO;
         BigDecimal noq = BigDecimal.valueOf(worker.queries().getExecutableQueryCount());
         Duration totalTime = Duration.ZERO;
         for (List<HttpWorker.ExecutionStats> datum : data) {
             for (HttpWorker.ExecutionStats exec : datum) {
-                if (exec.successful()) {
-                    successes = successes.add(BigDecimal.ONE);
+                if (exec.successful() || exec.timeout()) {
+                    executions = executions.add(BigDecimal.ONE);
                     totalTime = totalTime.plus(exec.duration());
                 }
             }
@@ -41,7 +41,7 @@ public class QMPH extends Metric implements TaskMetric, WorkerMetric {
         BigDecimal tt = (new BigDecimal(BigInteger.valueOf(totalTime.toNanos()), 9)).divide(BigDecimal.valueOf(3600), 20, RoundingMode.HALF_UP);
 
         try {
-            return successes.divide(tt, 10, RoundingMode.HALF_UP).divide(noq, 10, RoundingMode.HALF_UP).stripTrailingZeros();
+            return executions.divide(tt, 10, RoundingMode.HALF_UP).divide(noq, 10, RoundingMode.HALF_UP).stripTrailingZeros();
         } catch (ArithmeticException e) {
             return BigDecimal.ZERO;
         }
