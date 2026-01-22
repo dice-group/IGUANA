56fd22be2cb0fb1db43bc55f88cc0ad563a3c5e7
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2026-01-22 22:45:39 +0100

    Try to fix compilation error

--- src/main/java/org/aksw/iguana/cc/controller/MainController.java
+++ src/main/java/org/aksw/iguana/cc/controller/MainController.java
@@ -1,8 +1,10 @@
 package org.aksw.iguana.cc.controller;
 
 import com.beust.jcommander.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aksw.iguana.cc.suite.IguanaSuiteParser;
 import org.aksw.iguana.cc.suite.Suite;
+import org.aksw.iguana.cc.utils.system.SystemEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -98,6 +100,7 @@ public class MainController {
         String[] lines = properties.split("\\n");
         String projectVersion = lines[0].split("=")[1].trim();
         String ontologyVersion = lines[1].split("=")[1].trim();
+        final var sysEnv = SystemEnvironment.getSystemEnvironment().toString();
         System.out.printf("IGUANA version: %s (result ontology version: %s)%n", projectVersion, ontologyVersion);
     }
 
