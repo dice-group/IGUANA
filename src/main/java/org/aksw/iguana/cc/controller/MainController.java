b1f6bc9e2c12f018997a28e80b4ad4adc5fd24b0
Nick Molcanov <32801560+nck-mlcnv@users.noreply.github.com>
2026-01-23 13:44:38 +0100

    Add better version info

--- src/main/java/org/aksw/iguana/cc/controller/MainController.java
+++ src/main/java/org/aksw/iguana/cc/controller/MainController.java
@@ -100,8 +100,13 @@ public class MainController {
         String[] lines = properties.split("\\n");
         String projectVersion = lines[0].split("=")[1].trim();
         String ontologyVersion = lines[1].split("=")[1].trim();
-        final var sysEnv = SystemEnvironment.getSystemEnvironment().toString();
+        final var sysEnv = SystemEnvironment.getSystemEnvironment();
         System.out.printf("IGUANA version: %s (result ontology version: %s)%n", projectVersion, ontologyVersion);
+        System.out.println("System Environment:");
+        System.out.printf(" - OS: %s %s%n", sysEnv.osFamily(), sysEnv.osVersion());
+        System.out.printf(" - Java: %s %s%n", sysEnv.javaRuntimeName(), sysEnv.javaRuntimeVersion());
+        System.out.printf(" - CPU: %s%n", sysEnv.cpuName());
+        System.out.printf(" - Total RAM: %.2f GB%n", sysEnv.totalRam() / (1024.0 * 1024.0 * 1024.0));
     }
 
 }
